826986b7a99574ecfececd9305d335ff190dd654
jonnykira <jonny.donnelly@kirasystems.com>
2017-11-01 17:54:12 -0400

    change filereader in the Dataloader to read bytes

--- Utilities.py
+++ Utilities.py
@@ -2,7 +2,6 @@
 from __future__ import print_function
 import os
 import random
-import string
 import tensorflow as tf
 import time
 import os
@@ -13,55 +12,47 @@ import io
 class DataLoader():
 
     def __init__(self, data_dir, batch_size, seq_length):
+
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.seq_length = seq_length
 
-
         input_file = os.path.join(data_dir, "input.txt")
 
-        self.preprocess(input_file)
+        self.read_data(input_file)
         self.create_batches()
         self.reset_batch_pointer()
 
 
-    def preprocess(self,input_file):
+    def read_data(self, input_file):
 
-        with io.open(input_file,'r', encoding='utf-8') as f:
+        # read the input file as bytes
+        with io.open(input_file, 'rb') as f:
+            data = np.array(bytearray(f.read()), dtype='int32')
 
-            data = f.read() # data is a unicode string
-
-        # this is a list of utf-8 encoded bytes
-        bytes = list(data.encode('utf-8'))
-
-        # self.data is an array with bytes represented as integers
-        self.data = np.array([ord(x) for x in bytes])
-
+        self.data = data
 
     def create_batches(self):
+
         # calculate no. of batches, same as above
         self.num_batches = int(self.data.size / (self.batch_size * self.seq_length))
 
-        # When the data (tensor) is too small,
-        # let's give them a better error message
+        # give error message if tensor is too small
         if self.num_batches == 0:
             assert False, "Not enough data. Make seq_length and batch_size small."
 
-        # make the data into an equal number of batches, a small chunk of the data will probably be chopped off
-        # at the end
+        # make the data into an equal number of batches, a chunk of the data will probably be chopped off at the end
         self.data = self.data[:self.num_batches * self.batch_size * self.seq_length]
 
-        # ydata is 1-d numpy array containing the targets. This is a shifted version of x where the ith element of ydata is the i+1th element of xdata. The very last
+        # ydata is 1-d numpy array containing the targets. This is a shifted version of x where the ith element of ydata is the i+1th element of xdata and very last
         # element of ydata is the first element of xdata.
         xdata = self.data
         ydata = np.copy(self.data)
         ydata[:-1] = xdata[1:]
         ydata[-1] = xdata[0]
 
-        self.x_batches = np.split(xdata.reshape(self.batch_size, -1),
-                                  self.num_batches, 1)
-        self.y_batches = np.split(ydata.reshape(self.batch_size, -1),
-                                  self.num_batches, 1)
+        self.x_batches = np.split(xdata.reshape(self.batch_size, -1), self.num_batches, 1)
+        self.y_batches = np.split(ydata.reshape(self.batch_size, -1), self.num_batches, 1)
 
     # load the next batch and increment the counter
     def next_batch(self):
--- train_mLSTM.py
+++ train_mLSTM.py
@@ -5,7 +5,6 @@ import sys
 import numpy as np
 import argparse
 import random
-import string
 import tensorflow as tf
 import time
 import io
@@ -25,7 +24,7 @@ parser.add_argument('--rnn_size', type=int, default=128,
                     help='Size of RNN hidden states')
 parser.add_argument('--batch_size', type=int, default=32,
                     help='RNN minibatch size')
-parser.add_argument('--seq_length', type=int, default=64,
+parser.add_argument('--seq_length', type=int, default=32,
                     help='RNN sequence length')
 parser.add_argument('--num_epochs', type=int, default=1,
                     help='Number of training epochs')
@@ -39,13 +38,12 @@ parser.add_argument('--restore_path', type= str, default=None,
                     help='Path to a directory from which to restore a model from previous session')
 parser.add_argument('--summary_frequency', type=int, default=100,
                     help='Save tensorboard data every N steps')
-parser.add_argument('--sampling_frequency', type=int, default=1000,
+parser.add_argument('--sampling_frequency', type=int, default=100,
                     help='Generate samples from the model during training every N steps ')
 parser.add_argument('--num_chars', type=int, default=250,
                     help='Option to specify how many chars to sample from the model if --sampling_frequency is not zero ')
 parser.add_argument('--lr_decay', type=int, default=1,
                     help='Switch for learning rate decay. Integer argument of 1 for ON and 0 for OFF, learning rate is decayed to zero over the total number of updates')
-
 
 
 args = parser.parse_args()
@@ -57,8 +55,6 @@ embedding_size = args.embedding_size
 
 # preprocess the input data and create batches
 data_dir =args.data_dir
-
-
 
 loader = DataLoader(data_dir, batch_size, seq_length)
 
@@ -139,24 +135,25 @@ with graph.as_default():
     w = tf.Variable(tf.truncated_normal([rnn_size, vocabulary_size], -0.1, 0.1),name='Classifier_w')
     b = tf.Variable(tf.zeros([vocabulary_size]),name='Classifier_b')
 
-
+    # placeholder for the inputs and the targets
     inputs = tf.placeholder(tf.int32, shape=[batch_size, seq_length],name='inputs')
     targets = tf.placeholder(tf.int32, shape=[batch_size, seq_length],name='targets') # targets for the lm
 
+    # for the targets
     one_hot_labels = tf.one_hot(targets, vocabulary_size)
     labels_split_ = tf.split(one_hot_labels, seq_length, axis=1)
     list_labels = [tf.squeeze(input_, [1]) for input_ in labels_split_]
+
+    # for the inputs
     embedded_inputs  = tf.nn.embedding_lookup(W_embedding,inputs)
     inputs_split_ = tf.split(embedded_inputs, seq_length, axis=1)
     list_inputs = [tf.squeeze(input_, [1]) for input_ in inputs_split_]
-
 
     def mlstm_cell(x, h, c):
         """
         multiplicative LSTM cell. https://arxiv.org/pdf/1609.07959.pdf
 
         """
-
         # mt = (Wmxxt) ⊙ (Wmhht−1) - equation 18
         mt = tf.matmul(x,Wmx) * tf.matmul(h,Wmh)
         # hˆt = Whxxt + Whmmt
@@ -174,7 +171,6 @@ with graph.as_default():
 
         return h_new, c_new
 
-
     # Unrolled LSTM loop.
     outputs = list()
     output = saved_output # these are initially zero
@@ -184,9 +180,8 @@ with graph.as_default():
         outputs.append(output)
 
 
-    with tf.control_dependencies([saved_output.assign(output), # save the state between unrollings
-                                saved_state.assign(state)]):
-    # Classifier.
+    with tf.control_dependencies([saved_output.assign(output), saved_state.assign(state)]):
+        # Classifier.
         logits = tf.nn.xw_plus_b(tf.concat(outputs, 0), w, b)
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf.concat(list_labels, 0), logits=logits),name='loss')
         perplexity = tf.exp(loss)
@@ -201,23 +196,19 @@ with graph.as_default():
 
     # Predictions.
     train_prediction = tf.nn.softmax(logits)
-    print('pred shape',train_prediction.get_shape())
+
     # Sampling code.
     sample_input = tf.placeholder(tf.int32, shape=(1,), name = 'sample_input')
     sample_embedding= tf.nn.embedding_lookup(W_embedding,sample_input)
     saved_sample_output = tf.Variable(tf.zeros([1, rnn_size]),name = 'saved_sample_output')
     saved_sample_state = tf.Variable(tf.zeros([1, rnn_size]),name = 'saved_sample_state')
 
-    reset_sample_state = tf.group(
-    saved_sample_output.assign(tf.zeros([1, rnn_size])),
-    saved_sample_state.assign(tf.zeros([1, rnn_size])),name='reset_sample_state_op')
+    reset_sample_state = tf.group(saved_sample_output.assign(tf.zeros([1, rnn_size])), saved_sample_state.assign(tf.zeros([1, rnn_size])),name='reset_sample_state_op')
 
-    sample_output, sample_state = mlstm_cell(
-    sample_embedding, saved_sample_output, saved_sample_state)
+    sample_output, sample_state = mlstm_cell(sample_embedding, saved_sample_output, saved_sample_state)
 
-    with tf.control_dependencies([saved_sample_output.assign(sample_output),
-                                saved_sample_state.assign(sample_state)]):
-                                sample_prediction = tf.nn.softmax(tf.nn.xw_plus_b(sample_output, w, b), name = 'sample_prediction')
+    with tf.control_dependencies([saved_sample_output.assign(sample_output),saved_sample_state.assign(sample_state)]):
+        sample_prediction = tf.nn.softmax(tf.nn.xw_plus_b(sample_output, w, b), name = 'sample_prediction')
 
 # Summaries for tensorboard
 tf.summary.scalar('train_loss', loss)
@@ -241,13 +232,12 @@ with tf.Session(graph=graph) as session:
     summaries = tf.summary.merge_all()
     print('Summaries Merged')
 
+    # timestamp for saving the run
     timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
-    # writer for saving tensorboard logs
 
+    # writer for saving tensorboard logs
     writer = tf.summary.FileWriter(os.path.join(args.log_dir, timestamp))
     writer.add_graph(session.graph)
-
-
 
     for epoch in xrange(args.num_epochs):
 
@@ -263,8 +253,8 @@ with tf.Session(graph=graph) as session:
 
                 lr = init_lr-(((gs)*init_lr)/loader.num_batches) # linearly decay the learning rate to zero over the number of updates
 
-
                 _,l,perp,summary=session.run([optimizer, loss, perplexity, summaries], feed_dict={inputs:x, targets:y,learning_rate:lr})
+
             else:
 
                 _,l,perp,summary=session.run([optimizer, loss, perplexity, summaries], feed_dict={inputs:x, targets:y,learning_rate:init_lr})
@@ -272,9 +262,7 @@ with tf.Session(graph=graph) as session:
             end = time.time()
 
             print("Global step: {}, progress: ({}/{}), train_loss = {:.3f}, train_perplexity = {:.3f} time/batch = {:.3f}"
-                    .format(gs,epoch * loader.num_batches + batch,
-                    args.num_epochs * loader.num_batches,
-                     l, perp, end - start))
+                .format(gs,epoch * loader.num_batches + batch,args.num_epochs * loader.num_batches,l, perp, end - start))
 
             # write the summaries to the log_dir for tensorboard
             if args.summary_frequency != 0:
@@ -290,6 +278,7 @@ with tf.Session(graph=graph) as session:
                 if batch % args.sampling_frequency == 0:
                     start = time.time()
                     print('Sampling...')
+
                     feed = np.array(random.sample(xrange(vocabulary_size),1)) # random seed
 
                     sentence = unicode('')
