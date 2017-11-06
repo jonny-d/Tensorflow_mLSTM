b4917b355e0b84b3d06f7b21eb53f77ade8fc77f
jonnykira <jonny.donnelly@kirasystems.com>
2017-11-06 13:46:50 -0500

    fixed the extract_weights.py script to work with the updated train_mLSTM.py, with the redundant Wmb variable deleted.

--- extract_weights.py
+++ extract_weights.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import pprint
 import os
 import argparse
-
+import time
 
 parser = argparse.ArgumentParser(
                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -20,6 +20,10 @@ meta_path = args.meta_path
 model_path =args.model_dir
 output_file = args.output
 
+
+
+start = time.time()
+
 #restore a saved model then extract and save the weights in the appropriate format for encoder.py
 with tf.Session() as session:
 
@@ -28,6 +32,7 @@ with tf.Session() as session:
 
     # import the graph
     path = tf.train.get_checkpoint_state(args.model_dir)
+
     saver = tf.train.import_meta_graph(meta_path,clear_devices=True)
     print('graph imported')
 
@@ -54,10 +59,10 @@ with tf.Session() as session:
     np.save(output_file+'/0.npy', W_embedding)
 
     # these 4 matrices are (64,4096)
-    Wix = weights_list[7]
-    Wfx = weights_list[13]
-    Wox = weights_list[10]
-    Whx = weights_list[4]
+    Wix = weights_list[6]
+    Wfx = weights_list[12]
+    Wox = weights_list[9]
+    Whx = weights_list[3]
 
     # in encoder.py, these matrices are concatenated and saved as 1.npy
     # wx is the variable name in the encoder.py graph
@@ -66,16 +71,16 @@ with tf.Session() as session:
 
     # These matrices are saved seperately in .npy files, then concatenated from within the encoder.py script
     # before being used for the wh variable in the encoder.py graph. These are all of shape (4096,4096)
-    Wim = weights_list[8]
+    Wim = weights_list[7]
     np.save(output_file+'/2.npy', Wim)
 
-    Wfm = weights_list[14]
+    Wfm = weights_list[13]
     np.save(output_file+'/3.npy', Wfm)
 
-    Wom = weights_list[11]
+    Wom = weights_list[10]
     np.save(output_file+'/4.npy', Wom)
 
-    Whm = weights_list[5]
+    Whm = weights_list[4]
     np.save(output_file+'/5.npy', Whm)
 
 
@@ -90,10 +95,10 @@ with tf.Session() as session:
 
     # The bias variables are concatenated and saved in the 8.npy file
     # These are 4 (4096,) vectors used to calculate z in encoder.py
-    Wib = weights_list[9]
-    Wfb = weights_list[15]
-    Wob = weights_list[12]
-    Whb = weights_list[6]
+    Wib = weights_list[8]
+    Wfb = weights_list[14]
+    Wob = weights_list[11]
+    Whb = weights_list[5]
     b = np.concatenate((Wib,Wfb,Wob,Whb), axis=1)
     # remove singleton dimension
     b = b.squeeze()
@@ -101,34 +106,39 @@ with tf.Session() as session:
 
     # Coefficients used for weight normalizationn for the wx matrix, used in the calculation of z
     # the following vectores are conctenated and saved as 9.npy
-    gix = weights_list[20]
-    gfx = weights_list[24]
-    gox = weights_list[22]
-    ghx = weights_list[18]
+    gix = weights_list[19]
+    gfx = weights_list[23]
+    gox = weights_list[21]
+    ghx = weights_list[17]
     gx = np.concatenate((gix,gfx,gox,ghx))
     np.save(output_file+'/9.npy',gx)
 
 
     # Coefficients used for the  weight normalization for the wh matrix used in the calculation of z,
     # these are concatenated and saved as 10.npy
-    gim = weights_list[21]
-    gfm = weights_list[25]
-    gom = weights_list[23]
-    ghm = weights_list[19]
+    gim = weights_list[20]
+    gfm = weights_list[24]
+    gom = weights_list[22]
+    ghm = weights_list[18]
     gh = np.concatenate((gim,gfm,gom,ghm))
     np.save(output_file+'/10.npy',gh)
 
     # gmx and gmh are the weight normalization coefficients used for wmx and wmh in the calculation of m
     # gmx
-    gmx = weights_list[17]
+    gmx = weights_list[15]
     np.save(output_file+'/11.npy',gmx)
 
     # gmh
-    gmh = weights_list[18]
-    np.save(output_file+'/12.npy',gmx)
+    gmh = weights_list[16]
+    np.save(output_file+'/12.npy',gmh)
 
     # These aren't used for the representation extraction but extract the softmax weights too
-    Classifier_w = weights_list[26]
+    Classifier_w = weights_list[25]
     np.save(output_file+'/13.npy',Classifier_w)
-    Classifier_b = weights_list[27]
+    Classifier_b = weights_list[26]
     np.save(output_file+'/14.npy',Classifier_b)
+
+
+end = time.time()
+
+print('Weights extraction time: ' end - start )
